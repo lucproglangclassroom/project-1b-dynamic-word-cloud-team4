9d8c04915a2f23cd39f945c1bfeaf0c998e08a31
Leilaniriv <104607025+Leilaniriv@users.noreply.github.com>
2024-10-01 00:43:07 +0000

    Fixed Syntax Errors

--- src/test/scala/hellotest/StackTest.scala
+++ src/test/scala/hellotest/StackTest.scala
@@ -4,8 +4,9 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import scala.collection.mutable.Queue
 import scala.collection.immutable.Map // Use immutable Map to match the OutputSink trait
+import hellotest.{Main, OutputSink}
 
-class MainTest extends AnyFlatSpec with Matchers {
+class MainTest extends AnyFlatSpec {
 
   // Mock OutputSink for testing
   class MockOutputSink extends OutputSink {
@@ -17,76 +18,83 @@ class MainTest extends AnyFlatSpec with Matchers {
     }
   }
 
-  "wordcloud" should "generate correct word frequencies" in {
-    val mockOutput = new MockOutputSink
+  class MainTest extends AnyFlatSpec with Matchers {
+    "wordcloud" should "generate correct word frequencies" in {
+      val mockOutput = new MockOutputSink
 
-    val words = Iterator("hello", "world", "hello", "today", "world", "hello", "scala", "programming")
-    val cloudSize = 2
-    val minLength = 3
-    val windowSize = 5
-    val minFrequency = 2
+      val words = Iterator("hello", "world", "hello", "today", "world", "hello", "scala", "programming")
+      val cloudSize = 2
+      val minLength = 3
+      val windowSize = 5
+      val minFrequency = 2
 
-    // Run the wordcloud function
-    Main.wordcloud(words, cloudSize, minLength, windowSize, minFrequency, mockOutput)
+      // Run the wordcloud function
+      Main.wordcloud(words, cloudSize, minLength, windowSize, minFrequency, mockOutput)
 
-    // Check the printed word cloud
-    mockOutput.printedWordCloud should contain("hello")
-    mockOutput.printedWordCloud should contain("world")
-    mockOutput.printedWordCloud shouldNot contain("today")
-    mockOutput.printedWordCloud shouldNot contain("scala")
-  }
+      // Check the printed word cloud for correct frequencies
+      mockOutput.printedWordCloud shouldEqual Map("hello" -> 3, "world" -> 2)
 
-  it should "respect minimum word length" in {
-    val mockOutput = new MockOutputSink
 
-    val words = Iterator("hi", "hello", "there", "hi", "world")
-    val cloudSize = 2
-    val minLength = 4 // Set minimum length to 4
-    val windowSize = 5
-    val minFrequency = 1
+      // Check the printed word cloud
+      mockOutput.printedWordCloud should contain("hello")
+      mockOutput.printedWordCloud should contain("world")
+      mockOutput.printedWordCloud shouldNot contain("today")
+      mockOutput.printedWordCloud shouldNot contain("scala")
+    }
+  
 
-    // Run the wordcloud function
-    Main.wordcloud(words, cloudSize, minLength, windowSize, minFrequency, mockOutput)
+    it should "respect minimum word length" in {
+      val mockOutput = new MockOutputSink
 
-    // Check the printed word cloud
-    mockOutput.printedWordCloud should contain("hello")
-    mockOutput.printedWordCloud should contain("there")
-    mockOutput.printedWordCloud shouldNot contain("hi")
-    mockOutput.printedWordCloud shouldNot contain("world")
-  }
+      val words = Iterator("hi", "hello", "there", "hi", "world")
+      val cloudSize = 2
+      val minLength = 4 // Set minimum length to 4
+      val windowSize = 5
+      val minFrequency = 1
 
-  it should "handle an empty input" in {
-    val mockOutput = new MockOutputSink
+      // Run the wordcloud function
+      Main.wordcloud(words, cloudSize, minLength, windowSize, minFrequency, mockOutput)
 
-    val words = Iterator.empty
-    val cloudSize = 5
-    val minLength = 3
-    val windowSize = 10
-    val minFrequency = 1
+      // Check the printed word cloud
+      mockOutput.printedWordCloud should contain("hello")
+      mockOutput.printedWordCloud should contain("there")
+      mockOutput.printedWordCloud shouldNot contain("hi")
+      mockOutput.printedWordCloud shouldNot contain("world")
+    }
 
-    // Run the wordcloud function
-    Main.wordcloud(words, cloudSize, minLength, windowSize, minFrequency, mockOutput)
+    it should "handle an empty input" in {
+      val mockOutput = new MockOutputSink
 
-    // Check that the printed word cloud is empty
-    mockOutput.printedWordCloud shouldBe empty
-  }
+      val words = Iterator.empty
+      val cloudSize = 5
+      val minLength = 3
+      val windowSize = 10
+      val minFrequency = 1
 
-  it should "respect the window size" in {
-    val mockOutput = new MockOutputSink
+      // Run the wordcloud function
+      Main.wordcloud(words, cloudSize, minLength, windowSize, minFrequency, mockOutput)
 
-    val words = Iterator("apple", "banana", "apple", "orange", "banana", "banana", "grape")
-    val cloudSize = 3
-    val minLength = 3 // Set minimum length to 3
-    val windowSize = 4 // Set window size to 4
-    val minFrequency = 2
+      // Check that the printed word cloud is empty
+      mockOutput.printedWordCloud shouldBe empty
+    }
 
-    // Run the wordcloud function
-    Main.wordcloud(words, cloudSize, minLength, windowSize, minFrequency, mockOutput)
+    it should "respect the window size" in {
+      val mockOutput = new MockOutputSink
 
-    // Check the printed word cloud
-    mockOutput.printedWordCloud should contain("banana") // Should be counted
-    mockOutput.printedWordCloud shouldNot contain("apple") // Out of the window size
-    mockOutput.printedWordCloud shouldNot contain("orange") // Out of the window size
-    mockOutput.printedWordCloud shouldNot contain("grape") // Not enough frequency
+      val words = Iterator("apple", "banana", "apple", "orange", "banana", "banana", "grape")
+      val cloudSize = 3
+      val minLength = 3 // Set minimum length to 3
+      val windowSize = 4 // Set window size to 4
+      val minFrequency = 2
+
+      // Run the wordcloud function
+      Main.wordcloud(words, cloudSize, minLength, windowSize, minFrequency, mockOutput)
+
+      // Check the printed word cloud
+      mockOutput.printedWordCloud should contain("banana") // Should be counted
+      mockOutput.printedWordCloud shouldNot contain("apple") // Out of the window size
+      mockOutput.printedWordCloud shouldNot contain("orange") // Out of the window size
+      mockOutput.printedWordCloud shouldNot contain("grape") // Not enough frequency
+    }
   }
 }
