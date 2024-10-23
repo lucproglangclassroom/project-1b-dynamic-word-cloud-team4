5f0f72a74fd64901efaa5f1d52bdd631cfd210db
Tess Shirley <tshirley1@luc.edu>
2024-10-23 00:58:30 +0000

    1b changes

--- src/main/scala/hellotest/Main.scala
+++ src/main/scala/hellotest/Main.scala
@@ -2,35 +2,111 @@ package edu.luc.cs.consoleapp
 
 import scala.io.Source
 import org.apache.commons.collections4.queue.CircularFifoQueue
-import scala.util.{Try, Success, Failure}
-import mainargs._
+import scala.language.unsafeNulls
+import mainargs.{main, arg, ParserForMethods}
+import java.lang.System
+import scala.io.StdIn.{readLine, readInt}
 
 // Define a logging trait for diagnostics
 trait Logger {
   def log(message: String): Unit
 }
 
+trait OutputSink:
+  def print(wordCloud: Map[String, Int]): Unit
+
 object ConsoleLogger extends Logger {
   def log(message: String): Unit = println(message)
 }
 
-@main
-def topwords(
-    @arg(name = "last-n-words", short = 'n') lastNWords: Int = 10
-): Unit = {
-  val logger: Logger = ConsoleLogger
-  val queue = new CircularFifoQueue[String](lastNWords)
+class OutputToConsole extends OutputSink:
+  override def print(wordCloud: Map[String, Int]): Unit =
+    println("Word cloud: ")
+    wordCloud.foreach { case (word, count) =>
+      println(s"$word: $count")
+    }
 
-  val input = Source.stdin.getLines()
-  input.foreach { line =>
-    line.split("(?U)[^\\p{Alpha}0-9']+").foreach { word =>
-      if (word.nonEmpty) {
-        queue.add(word) // Automatically evicts the oldest word
-        logger.log(queue.toString) // Log the current state of the queue
-        if (System.out.checkError()) {
-          System.exit(1) // Handle SIGPIPE
+@main
+object Main {
+  def main(args: Array[String]): Unit = {
+    topwords(lastNWords = 10) // Call your main function with default arguments
+  }
+  
+  def run(
+    @arg(short = 'c', doc = "size of the sliding word cloud") cloudSize: Int = 10,
+    @arg(short = 'l', doc = "minimum word length to be considered") minLength: Int = 6,
+    @arg(short = 'w', doc = "size of the sliding FIFO queue") windowSize: Int = 1000,
+    @arg(short = 's', doc = "number of steps between word cloud updates") everyKSteps: Int = 10,
+    @arg(short = 'f', doc = "minimum frequency for a word to be included in the cloud") minFrequency: Int = 3
+  ): Unit = {
+  
+      println("Welcome to the Word Cloud Generator!")
+      println(s"Today's date: ${java.time.LocalDate.now}")
+      println("\nYou provided the following command-line arguments:")
+      println(s"- Cloud Size: $cloudSize")
+      println(s"- Minimum Word Length: $minLength")
+      println(s"- Window Size: $windowSize")
+      println(s"- Steps Between Updates: $everyKSteps")
+      println(s"- Minimum Frequency: $minFrequency")
+
+      // Create an instance of OutputToConsole
+      val output = new OutputToConsole
+
+      // Example words
+      val words = Iterator("programming", "scala", "programming", "language", "tomorrow","tomorrow", "hello", "world", "scala", "programming", "tomorrow", "programming", "functional")
+
+      // Call the wordcloud function with all required parameters
+      wordcloud(words, cloudSize, minLength, windowSize, minFrequency, output)
+  }
+
+  // Word cloud generation logic as functional
+  def wordcloud(
+      words: Iterator[String], 
+      cloudSize: Int, 
+      minLength: Int, 
+      windowSize: Int, 
+      minFrequency: Int,
+      output: OutputSink
+    ): Unit = {
+    
+    // sliding window implemented using scanLeft
+    val slidingWindows = words
+      .filter(_.length >= minLength) // filter by word length
+      .toList // convert to lsit for processing
+      .sliding(windowSize) // create sliding window
+
+    val freqMap = slidingWindows.foldLeft(Map.empty[String, Int]) {( acc,window) =>
+      window.foldLeft(acc) { (innerAcc, word) =>
+        if (word != null) {
+          val updatedCount = innerAcc.getOrElse(word, 0) + 1
+          innerAcc.updated(word, updatedCount) // create a new map with the updated frequency
+        } else {
+          innerAcc 
+        }
+    }
+  }
+    // Sort and print the word cloud
+    val cloud = freqMap.toSeq.sortBy(-_._2).take(cloudSize).toMap
+    if (cloud.nonEmpty) output.print(cloud)
+  }
+    //val cloud = freqMap.toSeq.sortBy(-_._2).take(cloudSize).toMap 
+   // println(s"Generated word cloud: $cloud")
+
+    //if(cloud.nonEmpty) output.print(cloud)
+  def topwords(
+    @arg(name = "last-n-words", short = 'n') lastNWords: Int = 10): Unit = {
+      val logger: Logger = ConsoleLogger
+      var queue = List.empty[String] // immutable List
+      val input = Source.stdin.getLines()
+      input.foreach { line =>
+        val words = line.split("(?U)[^\\p{Alpha}0-9']+").filter(_.nonEmpty)
+        words.foreach { word =>
+          queue = (word :: queue).take(lastNWords) // update the queue
+          logger.log(queue.toString) // Log the current state of the queue
+          if(System.out.checkError()) {
+            System.exit(1) // Handle SIGPIPE
+          }
         }
       }
     }
   }
-}
--- src/test/scala/hellotest/StackTest.scala
+++ src/test/scala/hellotest/StackTest.scala
@@ -3,11 +3,24 @@ package hellotest
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import scala.collection.immutable.Map // Use immutable Map to match the OutputSink trait
-import hellotest.{Main, OutputSink}
+import edu.luc.cs.consoleapp.Main
+import edu.luc.cs.consoleapp.OutputSink
 import java.{util => ju}
+import java.lang.System
+import java.nio.file.{Files, Paths, StandardOpenOption}
+import scala.io.Source
+import org.apache.commons.collections4.queue.CircularFifoQueue
+import scala.language.unsafeNulls
+import mainargs.{main, arg, ParserForMethods}
+import java.lang.System
+import scala.io.StdIn.{readLine, readInt}
+import java.nio.file.Path
+import java.nio.file.Files
+import java.nio.file.StandardOpenOption
 
-class MainTest extends AnyFlatSpec with Matchers {
 
+class StackTest extends AnyFlatSpec with Matchers {
+  
   // Mock OutputSink for testing
   class MockOutputSink extends OutputSink {
     var printedWordCloud: Map[String, Int] = Map()
@@ -17,179 +30,197 @@ class MainTest extends AnyFlatSpec with Matchers {
       printedWordCloud = wordCloud
     }
   }
+    "wordcloud" should "generate correct word frequencies" in {
+      val mockOutput = new MockOutputSink
 
-  "wordcloud" should "generate correct word frequencies" in {
-    val mockOutput = new MockOutputSink
+      val words = Iterator("hello", "world", "hello", "today", "world", "hello", "scala", "programming")
+      val cloudSize = 2
+      val minLength = 3
+      val windowSize = 8
+      val minFrequency = 2
 
-    val words = Iterator("hello", "world", "hello", "today", "world", "hello", "scala", "programming")
-    val cloudSize = 2
-    val minLength = 3
-    val windowSize = 8
-    val minFrequency = 2
+      // Run the wordcloud function
+      Main.wordcloud(words, cloudSize, minLength, windowSize, minFrequency, mockOutput)
 
-    // Run the wordcloud function
-    Main.wordcloud(words, cloudSize, minLength, windowSize, minFrequency, mockOutput)
+      // Expected frequencies
+      val expectedFrequencies = Map("world" -> 2, "hello" -> 3)
 
-    // Expected frequencies
-    val expectedFrequencies = Map("world" -> 2, "hello" -> 3)
-
-    // Check the printed word cloud for correct frequencies
-    mockOutput.printedWordCloud.toMap shouldEqual expectedFrequencies
-
-
-    // Check the printed word cloud
-    mockOutput.printedWordCloud.keys should contain allOf ("hello", "world")
-    mockOutput.printedWordCloud.keys should not contain ("today", "scala", "programming")
-  }
+      // Check the printed word cloud for correct frequencies
+      mockOutput.printedWordCloud.toMap shouldEqual expectedFrequencies
 
 
-  it should "respect minimum word length" in {
-    val mockOutput = new MockOutputSink
+      // Check the printed word cloud
+      mockOutput.printedWordCloud.keys should contain allOf ("hello", "world")
+      mockOutput.printedWordCloud.keys should not contain ("today", "scala", "programming")
+    }
 
-    val words = Iterator("hi", "hello", "there", "hi", "world")
-    val cloudSize = 2
-    val minLength = 4 // Set minimum length to 4
-    val windowSize = 5
-    val minFrequency = 1
+    it should "process words from a file correctly" in {
+      val mockOutput = new MockOutputSink
+      val testData = "hello world\nhello scala\nhello programming\nworld programming"
+      
+      val words = testData.split("\n").flatMap { line =>
+        line.split("(?U)[^\\p{Alpha}0-9']+").filter(word => word.nonEmpty)
+      }.iterator
 
-    // Run the wordcloud function
-    Main.wordcloud(words, cloudSize, minLength, windowSize, minFrequency, mockOutput)
+        val cloudSize = 2
+        val minLength = 3
+        val windowSize = 10
+        val minFrequency = 1
+        
+        // Run the wordcloud function
+        Main.wordcloud(words, cloudSize, minLength, windowSize, minFrequency, mockOutput)
+        
+        // Check expected frequencies
+        val expectedFrequencies = Map("hello" -> 3, "world" -> 2)
 
-    // Check the printed word cloud
-    mockOutput.printedWordCloud.keys should not contain ("hello")
-    mockOutput.printedWordCloud.keys should contain("there")
-    mockOutput.printedWordCloud.keys should contain ("world")
-    mockOutput.printedWordCloud.keys should not contain ("hi")
-  }
-
-  it should "handle an empty input" in {
-    val mockOutput = new MockOutputSink
-
-    val words = Iterator.empty
-    val cloudSize = 5
-    val minLength = 3
-    val windowSize = 10
-    val minFrequency = 1
-
-    // Run the wordcloud function
-    Main.wordcloud(words, cloudSize, minLength, windowSize, minFrequency, mockOutput)
-
-    // Check that the printed word cloud is empty
-    mockOutput.printedWordCloud shouldBe empty
-  }
-
-  it should "respect the window size" in {
-    val mockOutput = new MockOutputSink
-
-    val words = Iterator("apple", "banana", "apple", "orange", "banana", "banana", "grape")
-    val cloudSize = 3
-    val minLength = 3 // Set minimum length to 3
-    val windowSize = 4 // Set window size to 4
-    val minFrequency = 2
-
-    // Run the wordcloud function
-    Main.wordcloud(words, cloudSize, minLength, windowSize, minFrequency, mockOutput)
-
-    // Check the printed word cloud
-    mockOutput.printedWordCloud.keys should contain("banana") // Should be counted
-    mockOutput.printedWordCloud.keys should not contain ("apple", "orange", "grape")
+        // Assert the mock output contains expected results
+        mockOutput.printedWordCloud shouldEqual expectedFrequencies
+    }
     
+    it should "respect minimum word length" in {
+      val mockOutput = new MockOutputSink
+      val words = Iterator("hi", "hello", "there", "hi", "world")
+      val cloudSize = 2
+      val minLength = 4 // Set minimum length to 4
+      val windowSize = 5
+      val minFrequency = 1
+
+      // Run the wordcloud function
+      Main.wordcloud(words, cloudSize, minLength, windowSize, minFrequency, mockOutput)
+
+      // Check the printed word cloud
+      mockOutput.printedWordCloud.keys should not contain ("hello")
+      mockOutput.printedWordCloud.keys should contain("there")
+      mockOutput.printedWordCloud.keys should contain ("world")
+      mockOutput.printedWordCloud.keys should not contain ("hi")
+    }
+
+    it should "handle an empty input" in {
+      val mockOutput = new MockOutputSink
+
+      val words = Iterator.empty
+      val cloudSize = 5
+      val minLength = 3
+      val windowSize = 10
+      val minFrequency = 1
+
+      // Run the wordcloud function
+      Main.wordcloud(words, cloudSize, minLength, windowSize, minFrequency, mockOutput)
+
+      // Check that the printed word cloud is empty
+      mockOutput.printedWordCloud shouldBe empty
+    }
+
+    it should "respect the window size" in {
+      val mockOutput = new MockOutputSink
+
+      val words = Iterator("apple", "banana", "apple", "orange", "banana", "banana", "grape")
+      val cloudSize = 3
+      val minLength = 3 // Set minimum length to 3
+      val windowSize = 4 // Set window size to 4
+      val minFrequency = 2
+
+      // Run the wordcloud function
+      Main.wordcloud(words, cloudSize, minLength, windowSize, minFrequency, mockOutput)
+
+      // Check the printed word cloud
+      mockOutput.printedWordCloud.keys should contain("banana") // Should be counted
+      mockOutput.printedWordCloud.keys should not contain ("apple", "orange", "grape")
+    }
+
+    it should "respect the cloud size limit" in {
+      val mockOutput = new MockOutputSink
+
+      val words = Iterator("apple", "banana", "apple", "orange")
+      val cloudSize = 1
+      val minLength = 3
+      val windowSize = 10
+      val minFrequency = 1
+
+      // Run the wordcloud function
+      Main.wordcloud(words, cloudSize, minLength, windowSize, minFrequency, mockOutput)
+
+      // Check the printed word cloud respects the cloud size
+      mockOutput.printedWordCloud.size shouldEqual 1
+    }
+
+    it should "sort the word cloud by frequency" in {
+      val mockOutput = new MockOutputSink
+
+      val words = Iterator("apple", "banana", "apple", "banana", "apple", "orange")
+      val cloudSize = 2
+      val minLength = 3
+      val windowSize = 10
+      val minFrequency = 1
+
+      // Run the wordcloud function
+      Main.wordcloud(words, cloudSize, minLength, windowSize, minFrequency, mockOutput)
+
+      // Check the printed word cloud is sorted by frequency
+      val sortedWords = mockOutput.printedWordCloud.toSeq.sortBy(-_._2)
+      sortedWords shouldEqual Seq(("apple", 3), ("banana", 2))
+    }
+
+    it should "filter words by minimum frequency" in {
+      val mockOutput = new MockOutputSink
+
+      val words = Iterator("apple", "banana", "apple", "orange", "banana", "banana", "grape")
+      val cloudSize = 3
+      val minLength = 3
+      val windowSize = 10
+      val minFrequency = 3
+
+      // Run the wordcloud function
+      Main.wordcloud(words, cloudSize, minLength, windowSize, minFrequency, mockOutput)
+
+      // Check that only words with frequency >= minFrequency are included
+      mockOutput.printedWordCloud shouldEqual Map("banana" -> 3)
+    }
+
+    it should "remove words from the cloud when their frequency drops to zero" in {
+      val mockOutput = new MockOutputSink
+
+      val words = Iterator("apple", "apple", "orange", "apple", "banana", "banana", "banana")
+      val cloudSize = 3
+      val minLength = 3
+      val windowSize = 3 // Set small window size to trigger removals
+      val minFrequency = 1
+
+      // Run the wordcloud function
+      Main.wordcloud(words, cloudSize, minLength, windowSize, minFrequency, mockOutput)
+
+      // Verify the word "orange" is removed from the word cloud after it exits the window
+      mockOutput.printedWordCloud should not contain ("orange")
+    }
+
+    it should "parse command-line arguments correctly" in {
+      val args = Array("-c", "5", "-l", "4", "-w", "50", "-s", "5", "-f", "2")
+      Main.main(args)
+      // Add assertions to verify the behavior with the given arguments
+      val cloudSize = 5
+      val minLength = 4
+      val windowSize = 50
+      val stepsBetweenUpdates = 5
+      val minFrequency = 2
     
-  }
+      Main.main(args)
+    }
 
-  it should "respect the cloud size limit" in {
-    val mockOutput = new MockOutputSink
+    it should "handle large streams efficiently" in {
+      val mockOutput = new MockOutputSink
 
-    val words = Iterator("apple", "banana", "apple", "orange")
-    val cloudSize = 1
-    val minLength = 3
-    val windowSize = 10
-    val minFrequency = 1
+      // create a large stream of repeated words
+      val words = Iterator.fill(300)("banana") ++ Iterator.fill(300)("kiwi")
+      val cloudSize = 5
+      val minLength = 4
+      val windowSize = 400
+      val minFrequency = 1
 
-    // Run the wordcloud function
-    Main.wordcloud(words, cloudSize, minLength, windowSize, minFrequency, mockOutput)
+      // run the wordcloud function
+      Main.wordcloud(words, cloudSize, minLength, windowSize, minFrequency, mockOutput)
 
-    // Check the printed word cloud respects the cloud size
-    mockOutput.printedWordCloud.size shouldEqual 1
-  }
-
-  it should "sort the word cloud by frequency" in {
-    val mockOutput = new MockOutputSink
-
-    val words = Iterator("apple", "banana", "apple", "banana", "apple", "orange")
-    val cloudSize = 2
-    val minLength = 3
-    val windowSize = 10
-    val minFrequency = 1
-
-    // Run the wordcloud function
-    Main.wordcloud(words, cloudSize, minLength, windowSize, minFrequency, mockOutput)
-
-    // Check the printed word cloud is sorted by frequency
-    val sortedWords = mockOutput.printedWordCloud.toSeq.sortBy(-_._2)
-    sortedWords shouldEqual Seq(("apple", 3), ("banana", 2))
-  }
-
-  it should "filter words by minimum frequency" in {
-    val mockOutput = new MockOutputSink
-
-    val words = Iterator("apple", "banana", "apple", "orange", "banana", "banana", "grape")
-    val cloudSize = 3
-    val minLength = 3
-    val windowSize = 10
-    val minFrequency = 3
-
-    // Run the wordcloud function
-    Main.wordcloud(words, cloudSize, minLength, windowSize, minFrequency, mockOutput)
-
-    // Check that only words with frequency >= minFrequency are included
-    mockOutput.printedWordCloud shouldEqual Map("banana" -> 3)
-  }
-
-  it should "remove words from the cloud when their frequency drops to zero" in {
-    val mockOutput = new MockOutputSink
-
-    val words = Iterator("apple", "apple", "orange", "apple", "banana", "banana", "banana")
-    val cloudSize = 3
-    val minLength = 3
-    val windowSize = 3 // Set small window size to trigger removals
-    val minFrequency = 1
-
-    // Run the wordcloud function
-    Main.wordcloud(words, cloudSize, minLength, windowSize, minFrequency, mockOutput)
-
-    // Verify the word "orange" is removed from the word cloud after it exits the window
-    mockOutput.printedWordCloud should not contain ("orange")
-  }
-
-  it should "parse command-line arguments correctly" in {
-    val args = Array("-c", "5", "-l", "4", "-w", "50", "-s", "5", "-f", "2")
-    Main.main(args)
-    // Add assertions to verify the behavior with the given arguments
-    val cloudSize = 5
-    val minLength = 4
-    val windowSize = 50
-    val stepsBetweenUpdates = 5
-    val minFrequency = 2
-    
-    Main.main(args)
-  }
-
-  it should "handle large streams efficiently" in {
-    val mockOutput = new MockOutputSink
-
-    // create a large stream of repeated words
-    val words = Iterator.fill(300)("banana") ++ Iterator.fill(300)("kiwi")
-    val cloudSize = 5
-    val minLength = 4
-    val windowSize = 400
-    val minFrequency = 1
-
-    // run the wordcloud function
-    Main.wordcloud(words, cloudSize, minLength, windowSize, minFrequency, mockOutput)
-
-    // check that the word cloud handles large input without crashing
-    mockOutput.printedWordCloud.keys should contain ("banana")
-    mockOutput.printedWordCloud.keys should contain ("kiwi")
-  }
+      // check that the word cloud handles large input without crashing
+      mockOutput.printedWordCloud.keys should contain ("banana")
+      mockOutput.printedWordCloud.keys should contain ("kiwi")
+    }
 }
